db84b8af295fc4cf44c63fae9f097130271c0601
Paul <pspaul@users.noreply.github.com>
2021-08-18 15:30:41 +0200

    Fix path parsing

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -54,7 +54,7 @@ async function screenshot(url, format, viewportSize, dpr = 1, withJs = true) {
 // Based on https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/use-cases/13-returning-dynamic-images/functions/return-image.js
 async function handler(event, context) {
   // e.g. /https%3A%2F%2Fwww.11ty.dev%2F/small/1:1/smaller/
-  let pathSplit = event.path.split("/").filter(entry => !!entry);
+  let pathSplit = event.path.split("/").filter(entry => !!entry).slice(3);
   console.log('path:', pathSplit);
   let [url, size, aspectratio, zoom] = pathSplit;
   let format = "jpeg"; // hardcoded for now
