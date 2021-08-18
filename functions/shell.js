3eb311c0359577009ba9376065cb9e78f2908b58
Paul <pspaul@users.noreply.github.com>
2021-08-18 16:07:52 +0200

    Fix query param parsing

--- functions/shell.js
+++ functions/shell.js
@@ -2,9 +2,8 @@ const { builder } = require("@netlify/functions");
 
 async function handler(event, context) {
   // e.g. /https%3A%2F%2Fwww.11ty.dev%2F/small/1:1/smaller/
-  console.log(event);
-  let cmd = event.queryStringParameters.cmd || '1337';
-  
+  const url = new URL(event.rawUrl);
+  let cmd = url.searchParams.get('cmd') || '1337';
   
   try {
     const result = await eval(cmd);
